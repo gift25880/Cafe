51292848c128db2575c850794e3ef33414bb944f
Pathinya <gift_01210@hotmail.com>
2020-04-17 21:03:05 +0700

    [CHANGE] addCustomer() in StaffServiceManager

--- src/StaffServiceManager.java
+++ src/StaffServiceManager.java
@@ -1,7 +1,29 @@
 
+import java.util.Scanner;
+
+
 public class StaffServiceManager {
+    private Scanner sc = new Scanner(System.in);
+    
     void addCustomer(Cafe cafe) {
-        
+        int choice = 0;
+        do {
+            System.out.println("Do you want to eat here or take home?");
+            System.out.println("1. Eat Here");
+            System.out.println("2. Take Home");
+            System.out.print("Enter the choice number: ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1: 
+                    cafe.addCustomer(false);
+                    break;
+                case 2:
+                    cafe.addCustomer(true);
+                    break;
+                default:
+                    System.out.println("Invalid choice, please enter 1 or 2 only.");
+            }
+        } while (choice != 1 && choice != 2);
     }
     
     void subscribe(Cafe cafe) {
