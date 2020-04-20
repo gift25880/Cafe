a2e1b5a6d41cfb9628a45e28028b5637821eb120
Pathinya <gift_01210@hotmail.com>
2020-04-20 16:38:28 +0700

    [CHANGE] checkStock() in StaffServiceManager

--- src/StaffServiceManager.java
+++ src/StaffServiceManager.java
@@ -339,7 +339,47 @@ public class StaffServiceManager {
     }
 
     void checkStock(Cafe cafe) {
-
+        Item[][] menuList = cafe.getMenu();
+        int choice;
+        do {
+            System.out.println("What is the type of the menu you want to check the stock?");
+            System.out.println("1. Bakery");
+            System.out.println("2. Dessert");
+            System.out.println("3. Beverage");
+            System.out.println("0. Exit");
+            System.out.print("Enter choice number: ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1: 
+                    Item[] bakery = new Item[menuList[0].length];
+                    System.out.println("Bakery:");
+                    for (int i = 0; i < menuList[0].length; i++) {
+                        bakery[i] = menuList[0][i];
+                        System.out.println(bakery[i]);
+                    }
+                    break;
+                case 2: 
+                    Item[] dessert = new Item[menuList[1].length];
+                    System.out.println("Dessert:");
+                    for (int i = 0; i < menuList[1].length; i++) {
+                        dessert[i] = menuList[1][i];
+                        System.out.println(dessert[i]);
+                    }
+                    break;
+                case 3: 
+                    Item[] beverage = new Item[menuList[2].length];
+                    System.out.println("Beverage:");
+                    for (int i = 0; i < menuList[1].length; i++) {
+                        beverage[i] = menuList[2][i];
+                        System.out.println(beverage[i]);
+                    }
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Invalid choice, please enter 0-3 only");
+            }
+        } while (choice != 1 || choice != 2 || choice != 3 || choice != 0);
     }
 
     void restock(Cafe cafe) {
