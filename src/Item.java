a55c913e4ee525b2f4c7c2aeb84bd3968ae83be5
Kamolwish Woramethaleot <kamolwish.riew189521@mail.kmutt.ac.th>
2020-04-22 13:45:31 +0700

    [CHANGE] equal in Item

--- src/Item.java
+++ src/Item.java
@@ -1,34 +1,45 @@
 
 public class Item {
+
     private String id;
     private String name;
     private double price;
     private int stock;
-    
+
     public Item(String id, String name, double price, int stock) {
-        
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
     }
-    
+
     public String getName() {
         return this.name;
     }
-    
+
     public double getPrice() {
         return this.price;
     }
 
     public String getId() {
-        return id;
+        return this.id;
     }
-    
+
     public boolean equals(Item item) {
+        if (this == item) {
+            return true;
+        }
+        if (item == null) {
+            return false;
+        }
+        return true;
         
     }
-    
+
     public int getStock() {
         return this.stock;
     }
-    
+
     public void setStock(int amount) {
         this.stock = amount;
     }
