9e4d150b4fc5aaf31e27a9c6c2f7536f1db86e4c
Kamolwish Woramethaleot <kamolwish.riew189521@mail.kmutt.ac.th>
2020-04-22 13:47:05 +0700

    [CHANGE] equal in MenuItem

--- src/MenuItem.java
+++ src/MenuItem.java
@@ -1,33 +1,45 @@
 
 public class MenuItem {
+
     private Item item;
     private int amount;
-    
+
     public MenuItem(Item item, int amount) {
-        
+        this.item = item;
+        this.amount = amount;
+
     }
-    
+
     public void setItem(Item item) {
-        
+        this.item = item;
+
     }
-    
+
     public void addAmount(int amount) {
-        
+        this.amount = amount;
     }
-    
-    public void removeAmount(int amount){
-        
+
+    public void removeAmount(int amount) {
+        this.amount = amount;
     }
-    
+
     public Item getItem() {
-        
+        return this.item;
+
     }
-    
+
     public int getAmount() {
-        
+        return this.amount;
+
     }
-    
-    public boolean equals(MenuItem item){
-        
+
+    public boolean equals(MenuItem item) {
+        if (this == item) {
+            return true;
+        }
+        if (item == null) {
+            return false;
+        }
+        return true;
     }
 }
