074b26dc186466108d422d08610d8c9bd7113a1a
Pathinya <gift_01210@hotmail.com>
2020-04-26 15:05:47 +0700

    [CHANGE] PointPolicy

--- src/PointPolicy.java
+++ src/PointPolicy.java
@@ -1,5 +1,5 @@
 
 public interface PointPolicy {
     public static final int BATH_TO_ONE_POINT = 25;
-    public static final int POINT_TO_ONE_BATH = 100;
+    public static final int POINT_TO_ONE_BATH = 4;
 }
