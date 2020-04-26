587474306d6c8c8558498187e385d2d5cbab5382
Kamolwish Woramethaleot <kamolwish.riew189521@mail.kmutt.ac.th>
2020-04-26 14:24:04 +0700

    [CHANGE] Equal in Account

--- src/Account.java
+++ src/Account.java
@@ -35,6 +35,13 @@ public class Account {
     }
     
     public boolean equals(Object obj) {
+        if (this == obj) {
+        return true;
+        }
+        if (obj == null) {
+        return false;
+        }
+        return true;
         
     }
 }
