dd3a55a28bbcf859a6d0b3ee8f4e577ce8477313
Wlthr <napat.watt@mail.kmutt.ac.th>
2020-04-27 04:06:28 +0700

    [CHANGE] reduce maxTable

--- test/test.java
+++ test/test.java
@@ -6,7 +6,7 @@ import service.Cafe;
 
 public class test {
     public static void main(String[] args) {
-        Cafe cafe = new Cafe("All Cafe", 50);
+        Cafe cafe = new Cafe("All Cafe", 10);
         Cashier cashier = new Cashier(cafe);
         cashier.optionMenu();
     }
